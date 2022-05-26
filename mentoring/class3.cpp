e56ccb553063ac93fec619ed9576cb8873f3f0ad
sungwook <oceanstar7@kakao.com>
2022-05-27 00:36:36 +0900

    add class 3

---
+++ mentoring/class3.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Car {
+public:
+	string kind = "벤츠";
+	string color = "흰색";
+	int zeroback = 3;
+	void set_speed(int speed);	//미리선언 후 밖에서도 작성가능
+	int get_speed() {
+		return speed;
+	}
+	int get_zeroback();
+private:
+	int speed = 0;
+};
+
+int Car::get_zeroback() {
+	return zeroback;
+};
+void Car::set_speed(int speed) {
+	if (speed < 0)
+		this->speed = 0;		//잘못된 입력방지 가능
+	else
+		this->speed = speed;	//this = 파이썬의 self와 같은 것
+}
+
+int main(void)
+{
+	Car car;
+	int speed;
+	cin >> speed;	//속도입력
+	cout << car.get_speed() << endl;
+	car.set_speed(speed);
+	cout << car.get_speed() << endl;
+
+	return 0;
+}
